556ea26a171b01c50f74f0b643aa384518f8acd8
Gordon Woon <gordonwoon@live.com>
2022-10-28 12:15:09 +0800

    update all exif date formats

--- rename.py
+++ rename.py
@@ -2,19 +2,24 @@ from PIL import Image
 import sys
 import os
 import datetime
+import piexif
+import re
+import time
 
 # Set list of valid file extensions
 valid_extensions = [".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG"]
 
 # If folder path argument exists then use it
 # Else use the current running folder
-if len(sys.argv) < 1:
+if len(sys.argv) > 1:
     folder_path = sys.argv[1]
 else:
     folder_path = os.getcwd()
 
 # Get all files from folder
 file_names = os.listdir(folder_path)
+
+print("processing: " + folder_path)
 
 # For each file
 for file_name in file_names:
@@ -47,6 +52,34 @@ for file_name in file_names:
 
     # Close the image
     image.close()
+
+    match = re.match(r"^(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)", date_taken)
+    year = match.group(1)
+    month = match.group(2)
+    day = match.group(3)
+    hour = match.group(4)
+    minute = match.group(5)
+    second = match.group(6)
+
+    try:
+        if file_ext in [".png", ".PNG"]:
+            date = datetime.datetime(
+                year=int(year), month=int(month), day=int(day), hour=int(hour), minute=int(minute), second=int(second))
+            modTime = time.mktime(date.timetuple())
+            os.utime(old_file_path, (modTime, modTime))
+
+        else:
+            exif_dict = piexif.load(old_file_path)
+            # Update DateTimeOriginal
+            exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = date_taken
+            # Update DateTimeDigitized
+            exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = date_taken
+            # Update DateTime
+            exif_dict['0th'][piexif.ImageIFD.DateTime] = date_taken
+            exif_bytes = piexif.dump(exif_dict)
+            piexif.insert(exif_bytes, old_file_path)
+    except Exception as e:
+        print(file_name + ":" + e)
 
     # Reformat the date taken to "YYYYMMDD-HHmmss"
     date_time = date_taken \
